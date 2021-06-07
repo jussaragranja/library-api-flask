5533f6321bbd038f9e526d4eed9a9ba78eca940c
Jussara Granja <jussaragranja19@gmail.com>
2021-06-07 15:24:21 -0300

    add project

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.server.instance import server
+
+from src.controller.bookcontroller import *
+
+server.run()
---
+++ src/controller/bookcontroller.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_restplus import Api, Resource
+
+from src.server.instance import server
+
+app, api = server.app, server.api
+
+books_db = [
+    {'id': 0, 'title': 'War and Peace'},
+    {'id': 1, 'title': 'Clean Code'}
+]
+
+
+@api.route('/books')
+class BookList(Resource):
+    def get(self, ):
+        return books_db
+
+    def post(self, ):
+        response = api.payload
+        books_db.append(response)
+        return response, 200
+
+    def delete(self, ):
+        response = api.payload
+        books_db.remove(response)
+        return response, 200
